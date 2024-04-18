74dbf6f231c12182ca0660552250f6632fda9f42
slykid <slykid@naver.com>
2024-04-18 21:11:18 +0900

    feat: [Scala] 7. 객체지향프로그래밍 Ⅰ: 클래스

--- src/main/scala/Basic/OOP/BookTest.scala
+++ src/main/scala/Basic/OOP/BookTest.scala
@@ -5,13 +5,20 @@ object BookTest {
         val myBook1 = new Book(title="My Awesome Book1", author="Paul")
         val myBook2 = new Book(title="My Awesome Book2", author="Jason")
 
-        println(myBook1.author)
-        println(myBook1.title)
+        println("My Book1 author: " + myBook1.author)
+        println("My Book1 title: " + myBook1.title)
 
-        println()
+        myBook1.author = "Nancy"
+        println("My Book1 author: " + myBook1.author)
 
-        println(myBook2.author)
-        println(myBook2.title)
+        println(s"${myBook2.title} is written by ${myBook2.author}.")
+
+        val myBook3 = new Book(title="My awesome book 3")
+        println("My Book3 author: " + myBook3.author)
+
+        // Named Parameter (파이썬의 방식과 동일함)
+        val myBook4 = new Book(author="Steve", title="My awesome book 4")
+        println(s"${myBook4.title} is written by ${myBook4.author}.")
     }
 
 }
