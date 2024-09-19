2d7fa1505f45950031e7659ddf207f34c26e4451
slykid <slykid@naver.com>
2024-09-19 21:59:29 +0900

    feat: [Spark] 1. WordCount Test

--- src/main/scala/Spark/WordCount/WordCount.scala
+++ src/main/scala/Spark/WordCount/WordCount.scala
@@ -1,11 +1,16 @@
 package Spark.WordCount
 
+import org.apache.log4j.{Logger, Level}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
 
   def main(args: Array[String]) {
 
+    val logger = Logger.getLogger("WordCount")
+    logger.setLevel(Level.INFO)
+
+    logger.info("1. Spark Conf 설정")
     val conf = new SparkConf()
       .setMaster("local[*]")
       .setAppName("Spark/WordCount")
@@ -13,18 +18,22 @@ object WordCount {
     val sc = new SparkContext(conf)
 
     var exit_code = 1
-    try {
+    try
+    {
+      logger.info("2. README 파일 로드")
       var input = sc.textFile("input/README.md")
+
+      logger.info("3. WordCount 수행")
       var count = input.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
 
       count.saveAsTextFile("output/output.txt")
-      println("OK")
+      logger.info("OK")
 
       exit_code = 0
       
     } catch {
       case ex: Exception =>
-        println("ex:" + ex)
+        logger.error("ex:" + ex)
     } finally {
       System.exit(exit_code)
     }
