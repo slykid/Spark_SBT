23f63f237d15f718c04e6466c9aedca586470756
slykid <slykid@naver.com>
2025-02-05 23:20:25 +0900

    FEAT: [Scala] 2. 스칼라 기본 사용법 알기 * 스칼라 스크립트 작성법 * 함수 사용법 * 반복문 사용법

---
+++ src/main/scala/Basic/FirstStep/forargs.scala
@@ -0,0 +1,18 @@
+/**
+ * packageName  : Basic.FirstStep
+ * fileName     : forargs
+ * author       : kilhyunkim
+ * date         : 25. 2. 5.
+ * description
+ * ===================================================
+ * DATE                 AUTHOR              NOTE
+ * ---------------------------------------------------
+ * 25. 2. 5.          kilhyunkim         최초작성
+ *
+ */
+
+for(arg <- args) {
+  println(arg)
+}
+
+
